95d0d708e4fdec0ec9c536a34de234c1be4f8f6c
unknown <farukhayaz999@gmail.com>
2023-04-27 11:37:39 +0500

    Prepare for deployment

---
+++ frontend/build/precache-manifest.839aa6ca435b6f225a3e00bbe2a886cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cc1e7cca992afc9f245be5aadf7fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c97c7aba746ad7eac94",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c97c7aba746ad7eac94",
+    "url": "/static/js/main.89a5f833.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
